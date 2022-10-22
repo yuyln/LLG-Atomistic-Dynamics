2a0d2861bd49d1b0b0bd49d8fa516d5cde59865e
José Souza <josecarlos.bauru@gmail.com>
2022-10-22 02:52:30 -0300

    forgot

--- plot_anim_bin.py
+++ plot_anim_bin.py
@@ -34,11 +34,6 @@ ncols = nrow_ncol_steps[1]
 total = nrows * ncols
 frames = nrow_ncol_steps[2]
 
-raw_vecs = array.array("d")
-raw_vecs.frombytes(raw_data[12:])
-M = np.array(raw_vecs)
-mx_, my_, mz_ = M[0::3], M[1::3], M[2::3]
-
 xs, ys = zip(*[[j, i] for i in range(nrows) for j in range(ncols)])
 xs = np.array(xs)
 ys = np.array(ys)
@@ -59,12 +54,17 @@ y = y[y_in]
 facx = xu[1] - xu[0]
 facy = yu[1] - yu[0]
 
-
+vecsz = 3 * 8
 def GetBatch(index:int):
     if index >= frames: index = frames - 1
     elif index < 0: index = 0
-    mx__ = mx_[index * total:(index + 1) * total]
-    my__ = my_[index * total:(index + 1) * total]
+
+    raw_vecs = array.array("d")
+    raw_vecs.frombytes(raw_data[12 + index * total * vecsz: 12 + (index + 1) * total * vecsz])
+    M = np.array(raw_vecs)
+    mx_, my_, mz_ = M[0::3], M[1::3], M[2::3]
+    mx__ = mx_#[index * total:(index + 1) * total]
+    my__ = my_#[index * total:(index + 1) * total]
 
     mx = mx__[x_in]
     mx = mx[y_in]
@@ -72,7 +72,7 @@ def GetBatch(index:int):
     my = my__[x_in]
     my = my[y_in]
 
-    mz = mz_[index * total:(index + 1) * total]
+    mz = mz_#[index * total:(index + 1) * total]
     return mx * facx, my * facy, mz
 
 mx, my, mz = GetBatch(0)
@@ -102,19 +102,20 @@ ax.set_xlabel("$x(a)$", size=30)
 ax.set_ylabel("$y(a)$", size=30)
 
 
-vecs = ax.quiver(x, y, mx, my, angles='xy', scale_units='xy', pivot="mid", scale=1, width=0.002)
+# vecs = ax.quiver(x, y, mx, my, angles='xy', scale_units='xy', pivot="mid", scale=1, width=0.002)
 
 
 an = ax.scatter(col_ani, row_ani, color="green", s=20.0)
 pi = ax.scatter(col_pin, row_pin, color="yellow", s=20.0)
 ax.set_xlim([-0.5, ncols - 0.5])
 ax.set_ylim([-0.5, nrows - 0.5])
+# plt.show()
 
 def animate(i):
     mx, my, mz = GetBatch(i)
     mz = mz.reshape([nrows, ncols])
     img.set_array(mz)
-    vecs.set_UVC(mx, my)
+    # vecs.set_UVC(mx, my)
 
 ani = anim.FuncAnimation(fig, animate, frames=frames)
-ani.save("./videos/out_bin.mp4", fps=60, dpi=100)
+ani.save("./videos/out_bin.mp4", fps=60, dpi=250)
