cd7ae871cd3a433d25cf9bebdbf66ccfb36a2b32
Jose Souza <josecarlos.bauru@gmail.com>
2024-08-13 16:40:05 -0600

    felipe3

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void apply_circular_defect(grid *g, uint64_t row, uint64_t col, void *user_data)
     g->gp[y * cols + x].exchange *= 2.0;
 }
 
-int circular_defect(void) {
+int circular_defect(int radius) {
     grid g = grid_init(272, 272);
 
     int rows = g.gi.rows;
@@ -24,14 +24,11 @@ int circular_defect(void) {
 
     double dt = 0.01 * HBAR / (J * SIGN(J));
     double ratio = (double)rows / cols;
-    
-    integrate_params int_params = integrate_params_init();
-    int_params.field_func = create_field_D2_over_J(v3d_c(0, 0, 0.5), J, dm, mu);
-    int_params.dt = dt;
-    int_params.do_cluster = true;
-    int_params.interval_for_information = 1000;
-    int_params.interval_for_raw_grid = 0;
-    int_params.interval_for_rgb_grid = 50000;
+
+    string base_path = str_from_fmt("./data/%d", radius);
+    string command = str_from_fmt("mkdir -p %s", str_as_cstr(&base_path));
+    system(str_as_cstr(&command));
+    str_free(&command);
 
     grid_uniform(&g, v3d_c(0, 0, 1));
     unsigned int n = 6;
@@ -48,7 +45,16 @@ int circular_defect(void) {
         }
     }
 
-    grid_do_in_ellipse(&g, cols / 2.0, rows / 2.0, cols / 4.0, cols / 4.0, apply_circular_defect, NULL);
+    grid_do_in_ellipse(&g, cols / 2.0, rows / 2.0, radius, radius, apply_circular_defect, NULL);
+
+    integrate_params int_params = integrate_params_init();
+    int_params.field_func = create_field_D2_over_J(v3d_c(0, 0, 0.5), J, dm, mu);
+    int_params.dt = dt;
+    int_params.do_cluster = true;
+    int_params.output_path = base_path;
+    int_params.interval_for_information = 1000;
+    int_params.interval_for_raw_grid = 0;
+    int_params.interval_for_rgb_grid = 50000;
 
     int_params.duration = 10 * NS;
     integrate(&g, int_params);
@@ -57,14 +63,15 @@ int circular_defect(void) {
     int_params.current_func = create_current_stt_ac(2e10, 2e10, 1.0 / 20e-9, 0);
     integrate(&g, int_params);
 
-    organize_clusters("./clusters.dat", "./clusters_org.dat", cols * g.gp->lattice, rows * g.gp->lattice, 1e8);
-
     grid_free(&g);
     return 0;
 }
 
 int main(void) {
-    circular_defect();
+    double radii[] = {45, 50, 54, 59, 63, 68, 73, 77, 82, 86, 91};
+    int radii_size = sizeof(radii) / sizeof(*radii);
+    for (int i = 0; i < radii_size; ++i)
+        circular_defect(radii[i]);
     return 0;
 }
 
