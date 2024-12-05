19bbb7ebb177a2a5bc4be3c2b66297cbebc27951
Jose Souza <josecarlos.bauru@gmail.com>
2024-12-05 09:27:03 -0700

    modifications

--- build.sh
+++ build.sh
@@ -16,6 +16,10 @@ if test -f ./libatomistic3d.a; then
     rm ./libatomistic3d.a
 fi
 
+$CC $COMMON_CFLAGS create_kernel.c -o create_kernel
+./create_kernel ./src/kernel_files/tyche_i.c ./include/constants.h ./include/v3d.h ./include/grid_types.h ./src/kernel_files/random.h ./src/kernel_files/simulation_funcs.h ./include/colors.h ./src/v3d.c ./src/kernel_files/random.c ./src/kernel_files/simulation_funcs.c ./src/colors.c ./src/kernel_files/kernel.c
+rm ./create_kernel
+
 if [ "$1" = "install" ]; then
     if test -d $HOME/.local/lib/atomistic3d; then
         rm -r $HOME/.local/lib/atomistic3d
@@ -23,10 +27,6 @@ if [ "$1" = "install" ]; then
     mkdir --parents $HOME/.local/lib/atomistic3d
     mkdir --parents $HOME/.local/bin
     CFLAGS="$COMMON_CFLAGS"
-
-    $CC $CFLAGS create_kernel.c -o create_kernel
-    ./create_kernel ./kernel_files/tyche_i.c ./include/constants.h ./include/v3d.h ./include/grid_types.h ./kernel_files/random.h ./kernel_files/simulation_funcs.h ./include/colors.h ./src/v3d.c ./kernel_files/random.c ./kernel_files/simulation_funcs.c ./src/colors.c ./kernel_files/kernel.c
-    rm ./create_kernel
 
     $CC -fPIC $CFLAGS -c $FILES $LIBS
 
@@ -41,12 +41,7 @@ if [ "$1" = "install" ]; then
     $CC -fPIC $CFLAGS -shared -o libatomistic3d.so $FILES $LIBS
     cp ./libatomistic3d.so $HOME/.local/lib/atomistic3d/
 else
-    set -xe
     CFLAGS="$COMMON_CFLAGS -Wall -Wextra -pedantic -ggdb -g3 -Wno-overlength-strings -Wno-override-init"
-
-    $CC $CFLAGS create_kernel.c -o create_kernel
-    ./create_kernel ./kernel_files/tyche_i.c ./include/constants.h ./include/v3d.h ./include/grid_types.h ./kernel_files/random.h ./kernel_files/simulation_funcs.h ./include/colors.h ./src/v3d.c ./kernel_files/random.c ./kernel_files/simulation_funcs.c ./src/colors.c ./kernel_files/kernel.c
-    rm ./create_kernel
 
     $CC $CFLAGS -c $FILES $LIBS
 
